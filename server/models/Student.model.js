3c70257bc3e73b657db209c9cc46f0cf0428bd76
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-13 15:46:43 +0200

    create Student model

---
+++ server/models/Student.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    background: {type: String, required: true},
+    cohort: {type: Number, required: true},
+    email: {type: String, required: true},
+    firstName: {type: String, required: true},
+    image: {type: String, required: true},
+    languages: {type: [String], required: true},
+    lastName: {type: String, required: true},
+    linkedinUrl: {type: String, required: true},
+    phone: {type: String, required: true},
+    program: {type: String, required: true},
+    projects: {type: Array, required: true}
+})
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
